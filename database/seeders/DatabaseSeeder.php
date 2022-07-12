c49d2a3e752772579a873d7668fc9883bc07094f
Quan Troy 19 <doquan285lg@gmail.com>
2022-07-12 14:10:27 +0700

    bai1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // Tạo dữ liệu test
+        $dataStudentSeed=[
+            ['name'=>'Do Minh Quan',
+           'created_at'=>date('Y-m-dH:i:s'),
+           'updated_at'=>date('Y-m-dH:i:s'),
+           ],
+        ];
+        for($i=0; $i<100; $i++){
+            array_push($dataStudentSeed,
+                ['name'=>'Do Minh Quan ' . $i,
+               // có thể bỏ trống vì đã dùng nullable
+                'created_at'=>date('Y-m-dH:i:s'),
+                'updated_at'=>date('Y-m-dH:i:s'),
+            ],);
+        }
+        DB::table('users')->insert($dataStudentSeed);
     }
 }
